5e734f4de914f9d0e94df5f016c7df497d5f4ff7
Ammar <ammar@ovasave.com>
2025-09-27 17:50:20 +0400

    metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,18 @@
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-async function ProductDetailsPage({ params }: { params: Promise<{ productId: string }> }) {
+type Props = {
+  params: Promise<{ productId: string }>;
+};
+
+export async function generateMetadata(parmas: Props): Promise<Metadata> {
+  const params = await parmas.params;
+  return {
+    title: `Product ${params.productId}`,
+  };
+}
+
+async function ProductDetailsPage({ params }: Props) {
   //   console.log("params", params);
   const { productId } = await params;
   if (productId === "2") {
