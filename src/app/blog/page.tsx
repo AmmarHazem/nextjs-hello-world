19d51ac64090f4a1fcd47279b431c39272d95939
Ammar <ammar@ovasave.com>
2025-09-28 19:29:01 +0400

    title tag

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import { FC } from "react";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog",
+  },
+};
 
 const BlogPage: FC = () => {
   return <div>BlogPage</div>;
